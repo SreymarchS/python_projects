282ec709638248fd66864775987d0afd9d5efc2c
Sreymarch Soth <sreymarch.soth@gmail.com>
2024-11-05 20:10:09 +1100

    make the  message more clear

--- Pattern.py
+++ Pattern.py
@@ -1,7 +1,7 @@
 #creating pattern loop
 
 rows = int(input("Please enter the number of the rows: "))
-characters = (input("Please enter the character: ")
+characters = (input("Please enter the character to use: ")
 
 for rows in range(1, rows + 1):
     print(characters * rows)
